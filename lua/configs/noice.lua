93015cdd8dbef76ca25f3d34a243ee7bbb737aa8
Vlados <vlad.kznetsov@gmail.com>
2025-03-03 21:15:53 +0300

    Fixed noice config

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -3,6 +3,44 @@ local options = {
     bottom_search = true,
     lsp_doc_border = true,
   },
+  lsp = {
+    signature = {
+      enabled = true,
+      auto_open = {
+        enabled = false,
+      },
+    },
+  },
+  views = {
+    cmdline_popup = {
+      position = {
+        row = 5,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
+      },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = 8,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
+    },
+  },
 }
 
 return options
