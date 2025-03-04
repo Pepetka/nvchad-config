750ac8f3e1471a0a1cb0c812c3990634e0b0cc83
Vlados <vlad.kznetsov@gmail.com>
2025-03-04 20:25:44 +0300

    Improved css injection for css-in-js

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -14,3 +14,17 @@
   (#set! injection.include-children)
   (#set! injection.language "styled"))
 
+; styled(Component)<{ foo: "bar" }>`<css>`
+(call_expression
+  function: (non_null_expression
+    (instantiation_expression
+      (call_expression
+        function: (identifier) @_name
+        arguments: (arguments(identifier)) @_prop
+        (#eq? @_name "styled"))
+      type_arguments: (type_arguments)?))
+  arguments: (template_string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "styled"))
+
